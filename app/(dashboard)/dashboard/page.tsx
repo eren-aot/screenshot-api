e521a71c04e0f0bc4a579d6013b10adf98a977a2
wolfgunblood <jalajdorai100@gmail.com>
2024-01-16 18:54:18 +0530

    Changed the location

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,30 +1,11 @@
-"use client"
-
-import { Button } from '@/components/ui/button'
 import React from 'react'
-import URLForm from '../_components/URLForm'
-import ScreenshotReturn from '../_components/ScreenshotReturn'
 
 type Props = {}
 
-const DashboardPage = (props: Props) => {
-
-  // const user = userStore((state: any) => state.user)
-  // const updateuser = userStore((state: any) => state.updateUser)
-
+const PlaygroundPage = (props: Props) => {
   return (
-    <div className='h-full flex flex-col'>
-      <h1 className='text-lg font-semibold pt-5 pl-5'>Playground</h1>
-      <div className='flex p-5 justify-between'>
-        <div className='flex flex-col'>
-          <URLForm />
-        </div>
-        <div className='flex flex-col'>
-          <ScreenshotReturn />
-        </div>
-      </div>
-    </div>
+    <div>PlaygroundPage</div>
   )
 }
 
-export default DashboardPage
+export default PlaygroundPage
--- app/(dashboard)/playground/page.tsx
+++ app/(dashboard)/playground/page.tsx
@@ -1,11 +1,30 @@
+"use client"
+
+import { Button } from '@/components/ui/button'
 import React from 'react'
+import URLForm from '../_components/URLForm'
+import ScreenshotReturn from '../_components/ScreenshotReturn'
 
 type Props = {}
 
-const PlaygroundPage = (props: Props) => {
+const DashboardPage = (props: Props) => {
+
+  // const user = userStore((state: any) => state.user)
+  // const updateuser = userStore((state: any) => state.updateUser)
+
   return (
-    <div>PlaygroundPage</div>
+    <div className='h-full flex flex-col'>
+      <h1 className='text-lg font-semibold pt-5 pl-5'>Playground</h1>
+      <div className='flex p-5 justify-between'>
+        <div className='flex flex-col'>
+          <URLForm />
+        </div>
+        <div className='flex flex-col'>
+          <ScreenshotReturn />
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default PlaygroundPage
+export default DashboardPage
